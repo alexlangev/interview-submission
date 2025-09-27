7398026794ede7b0bdef7483fd9e9c646481248d
Alex Langevin <alexlangev@gmail.com>
2025-09-27 18:39:15 -0400

    logging middleware

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,12 +7,14 @@ import (
 	v1 "github.com/alexlangev/interview-submission/internal/api/v1/income-tax"
 	"github.com/alexlangev/interview-submission/internal/core"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(calc *core.Calculator) http.Handler {
 	r := chi.NewRouter()
 
-	//
+	r.Use(middleware.Logger)
+
 	r.Get("/health", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
